77f1960ff1945cbd72de681b9e1cba41aa144d56
Mert Osanmaz <mosanmaz@danusports.com>
2024-01-28 01:20:48 +0000

    gsap func

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,101 @@ import LogoIcon from './components/LogoIcon'
 import CameraIcon from './components/CameraIcon'
 import MagnifierIcon from './components/MagnifierIcon'
 import ListItemSkeleton from './components/ListItemSkeleton'
-
+import BagIcon from './components/BagIcon'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 function App() {
+
+  const headerRef = useRef(null);
+  const searchControlsRef = useRef(null);
+  const logoRef = useRef(null);
+  const searchWrapperRef = useRef(null);
+  const sneakyBearRef = useRef(null);
+
+  useEffect(() => {
+    if (!CSS.supports('animation-timeline: scroll()')) {
+      gsap.registerPlugin(ScrollTrigger);
+      const HEADER = headerRef.current;
+
+      if (HEADER) {
+        gsap.to(searchControlsRef.current, {
+          width: HEADER.offsetWidth - 66,
+          scrollTrigger: {
+            scrub: 0.25,
+            ease: 'none',
+            start: 0,
+            end: HEADER.offsetHeight
+          }
+        });
+
+        gsap.to(logoRef.current, {
+          yPercent: -25,
+          opacity: 0,
+          scrollTrigger: {
+            scrub: 0.25,
+            ease: 'none',
+            start: 0,
+            end: HEADER.offsetHeight * 0.8
+          }
+        });
+
+        gsap.to(searchWrapperRef.current, {
+          boxShadow: '0 5px 10px hsl(0 0% 0%)',
+          scrollTrigger: {
+            scrub: 0.25,
+            ease: 'none',
+            start: HEADER.offsetHeight,
+            end: HEADER.offsetHeight * 1.5
+          }
+        });
+
+        gsap.to(searchWrapperRef.current, {
+          background: 'var(--bg)',
+          scrollTrigger: {
+            scrub: 0.25,
+            ease: 'none',
+            start: HEADER.offsetHeight,
+            end: HEADER.offsetHeight * 1.5
+          }
+        });
+
+        gsap.to(sneakyBearRef.current, {
+          transform: 'rotate(90deg) translateX(calc(var(--header-height) * 0.125)) translateY(50%)',
+          scrollTrigger: {
+            scrub: 0.25,
+            ease: 'none',
+            start: HEADER.offsetHeight * 1.5,
+            end: HEADER.offsetHeight * 2
+          }
+        });
+      }
+    }
+  }, []);
+
+
 
   return (
     <>
  <div>
     <a href="#">
       <span className="sr-only">Back to top</span>  
-      <BackToTopIcon />
+      <BackToTopIcon ref={sneakyBearRef} />
     </a>
-    <a href="#" className="logo">
+    <a href="#" className="logo" ref={logoRef}>
       <LogoIcon />
     </a>
     <div className="header-wrapper">
-      <header>
+      <header ref={headerRef}>
         <button>
-          <CameraIcon />
+          <BagIcon />
         </button>
       </header>
     </div>
-    <div className="search-wrapper">
+    <div className="search-wrapper" ref={searchWrapperRef}>
       <div className="search-bar">
-        <div className="search-controls">
+        <div className="search-controls" ref={searchControlsRef}>
           <button className="camera">
             <CameraIcon />
           </button>
